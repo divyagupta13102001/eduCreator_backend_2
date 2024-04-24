9019e8feb9311bc30ec631c3d690fbfe49c6ac2f
divya <divya@gmail.com>
2024-04-24 20:20:42 +0530

    fixed an error

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // User Signup
 
