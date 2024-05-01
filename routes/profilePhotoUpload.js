8212863e0ddebbc64afefc27d4eb5e64da38dfce
divya <divya@gmail.com>
2024-05-02 01:49:02 +0530

    changes

--- routes/profilePhotoUpload.js
+++ routes/profilePhotoUpload.js
@@ -110,4 +110,26 @@ router.post('/upload-Id/:userId', upload.single('photo'), async (req, res) => {
         res.status(500).json({ message: 'Server Error' });
     }
 });
+router.get('/idCard/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+
+        // Find user by ID
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Check if the user has uploaded an ID card
+        if (!user.identityCard) {
+            return res.status(404).json({ message: 'ID card not found for this user' });
+        }
+
+        res.status(200).json({ identityCard: user.identityCard });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
 module.exports = router;
